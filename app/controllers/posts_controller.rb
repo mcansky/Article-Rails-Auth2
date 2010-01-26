27667fdcd12614a647d8e5d65ba9012677635613
T Riboulet <riboulet@gmail.com>
2010-01-26 23:40:13 +0100

    initial code to manage posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,44 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+  
+  def show
+    @post = Post.find(params[:id])
+  end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(params[:post])
+    if @post.save
+      flash[:notice] = "Post créé avec succès."
+      redirect_to @post
+    else
+      render :action => 'new'
+    end
+  end
+  
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(params[:post])
+      flash[:notice] = "Post mis à jour avec succès."
+      redirect_to @post
+    else
+      render :action => 'edit'
+    end
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:notice] = "Post détruit avec succès."
+    redirect_to posts_url
+  end
+end
