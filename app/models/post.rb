a7d053bab4ad5599d8f060254cd618eda3c714c6
T Riboulet <riboulet@gmail.com>
2010-01-28 19:11:44 +0100

    ajout de l'association posts <-> users

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
   attr_accessible :title, :content
+	belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,13 +9,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100126231052) do
+ActiveRecord::Schema.define(:version => 20100127205508) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
