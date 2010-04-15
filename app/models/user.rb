070f3752c85f769cce4741caba41e8234f2debcc
T Riboulet <riboulet@gmail.com>
2010-04-15 09:29:35 +0200

    ajout des methodes is_author? et is_moderator?

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
 	acts_as_authentic
 	has_many :posts
+
+	def is_author?
+    return true if role.to_sym == :auteur
+    return false
+  end
+  def is_moderator?
+    return true if role.to_sym == :moderateur
+    return false
+  end
 end
